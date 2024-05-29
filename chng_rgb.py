71533ff1ea4a758aa49b1ba4c952eff89932dab7
Farman Ullah <fu221135@gmail.com>
2024-05-29 18:04:33 +0500

    Update chng_rgb.py

--- chng_rgb.py
+++ chng_rgb.py
@@ -2,7 +2,7 @@ import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
 import numpy as np
 
-url = "D:\\#PICS\\Camera Roll\\2023_04_03_14_52_IMG_0811.JPG"
+url = "# add the image url"
 pic = mpimg.imread(url)
 
 # Display the original image
